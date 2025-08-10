0745bd70dc06d2e11885c10c9533c81e571ef341
Youzhi <youzhi.wang.1029@outlook.com>
2025-08-11 03:13:34 +0800

    ..

--- src/pages/PDFViewer/PDFViewer.tsx
+++ src/pages/PDFViewer/PDFViewer.tsx
@@ -9,6 +9,8 @@ import type { ToolType, ColorType } from "../../components/AnnotationToolbar";
 import { renderAnnotationsForPageWithData } from "./renderAnnotationsForPageWithData";
 import type { AnnotationType } from "./types";
 import AnnotationCard from "./AnnotationCard";
+import annotationMockData from "./data/annotation-mock-data";
+
 // Configure the worker
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
@@ -52,34 +54,7 @@ function PDFViewer() {
   const [selectedTool, setSelectedTool] = useState<ToolType>("cursor");
   const [selectedColor, setSelectedColor] = useState<ColorType>("#FFFF00");
 
-  const [annotations, setAnnotations] = useState<AnnotationType[]>([
-    {
-      id: "1754623246144-0.4475432192452905",
-      pageNumber: 1,
-      type: "highlight",
-      x: 475.5234375,
-      y: 504.1328125,
-      width: 358.9285888671875,
-      height: 29.96875,
-      content: " productivity\napplications",
-      comment: "用词不准确",
-      color: "rgba(255, 255, 0, 0.4)",
-      pathData:
-        "M 292.7947998046875 0 L 358.9285888671875 0 L 358.9285888671875 14.96875 L 66.7435302734375 14.96875 L 66.7435302734375 29.96875 L 0 29.96875 L 0 15 L 292.7947998046875 15 Z",
-    },
-    {
-      id: "1754623271644-0.8872989495648059",
-      pageNumber: 2,
-      type: "rectangle",
-      x: 468.90625,
-      y: 98,
-      width: 372,
-      height: 102,
-      content: "",
-      comment: "代码无法跑通",
-      color: "#FF0000",
-    },
-  ]);
+  const [annotations, setAnnotations] = useState<AnnotationType[]>([...annotationMockData]);
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   // Rectangle drawing state
